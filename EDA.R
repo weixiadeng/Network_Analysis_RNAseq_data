fea15d170f58004ff6f7b5f6abf7415c24c051d2
Weixia Deng <wdeng@iastate.edu>
2023-12-01 17:17:12 -0600

    Update

--- EDA.R
+++ EDA.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(GEOquery)
+library(factoextra)
+library(FactoMineR)
 
 # Access file path
 fileList <- list.files("./Data/")
@@ -11,6 +13,6 @@ dat.annot <- getGEO(filename = filePath[2])
 df.soft <- Table(dat.soft)
 df.annot <- Table(dat.annot)
 df.txt <- read.table(filePath[3], sep = "\t", header = TRUE, fill = TRUE)
-
+# Analysis of "GDS2808.soft"
 disease.state <- dat.soft@dataTable@columns$disease.state
-
+soft.pca <- PCA(df.soft[,2:ncol(df.soft)], graph = FALSE)
