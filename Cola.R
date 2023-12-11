517abc565c48579349eba7dcc3aa0be5cfb0ea29
Weixia Deng <wdeng@iastate.edu>
2023-12-11 01:50:49 -0600

    Update

---
+++ Cola.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(GEOquery)
+library(cola)
+
+gds <- getGEO(filename = "./Data/GDS2808.soft")
+eset <- GDS2eSet(gds)
+mat <- exprs(eset)
+anno <- gds@dataTable@columns
+
+# one patient from the sepsis group with > 80% missing data was removed
+idx.col <- c(); ngene <- nrow(mat)
+for (i in 1:ncol(mat)){
+  idx.col[i] <- (is.na(mat[,i]) %>% sum()) / ngene
+}
+mat <- mat[,!(idx.col > 0.8)]
+anno <- anno[!(idx.col > 0.8),]
+
+mat <- adjust_matrix(mat)
+res_list <- run_all_consensus_partition_methods(mat, mc.cores = 4, anno = anno)
+cola_report(res_list, "Cola_report.html")
