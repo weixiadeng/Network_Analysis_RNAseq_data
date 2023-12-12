66aa803b12c67cc82f9f43d9924c7dbc7a25e389
Weixia Deng <wdeng@iastate.edu>
2023-12-12 01:20:48 -0600

    Update

--- test_1.R
+++ test_1.R
@@ -3,15 +3,27 @@ library(GEOquery)
 library(org.Hs.eg.db)
 library(clusterProfiler)
 
-gseData <- getGEO("GSE236099", GSEMatrix = TRUE)
+df.labels <- read.csv("./Data/GSE185263_raw_file_labels.csv")
+df <- read.csv("./Data/GSE185263_raw_counts.csv.gz")
 
-# reading in data from deseq2
-df <- Table(gse)
 # we want the log2 fold change 
 original_gene_list <- df$log2FoldChange
 # name the vector
 names(original_gene_list) <- df$X
 # omit any NA values 
-gene_list <- na.omit(original_gene_list)
+gene_list <- df$ensembl_gene_id
 # sort the list in decreasing order (required for clusterProfiler)
-gene_list = sort(gene_list, decreasing = TRUE)
+gene_list <- sort(gene_list, decreasing = TRUE)
+
+keytypes(org.Hs.eg.db)
+
+gse <- gseGO(geneList = "ENSG00000171984", 
+             ont = "ALL", 
+             keyType = "ENSEMBL", 
+             nPerm = 10000, 
+             minGSSize = 3, 
+             maxGSSize = 800, 
+             pvalueCutoff = 0.05, 
+             verbose = TRUE, 
+             OrgDb = org.Hs.eg.db, 
+             pAdjustMethod = "none")
